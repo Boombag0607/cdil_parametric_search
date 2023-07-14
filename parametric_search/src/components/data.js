8c3a0953cd13cf65057ae9a4c4d7786cde51bcde
Ananya Gautam <ananyagautam0607@gmail.com>
2023-07-14 18:18:10 +0530

    change to class component
    Signed-off-by: Ananya Gautam <ananyagautam0607@gmail.com>

--- parametric_search/src/components/data.js
+++ parametric_search/src/components/data.js
@@ -1,32 +1,96 @@
-import { useState } from "react";
+// import { useState } from "react";
+import "./data.css";
+import { Component } from "react";
 
-function Data(props) {
-  const [selected, setSelected] = useState(true);
-  let key = props.key;
-  let data = props.data;
-  return (
-    <div
-      key={key}
-      data={data}
-      style={{
-        borderTop: "1px solid #ccc",
-        width: "12em",
-        height: "2em",
-        overflowY: "scroll",
-        hover: "pointer",
-        hoverColor: "blue",
-      }}
-      onClick={() => {
-        setSelected(!selected);
-        console.log(selected)
-      }}
-    >
-      {/*Make DataBase API Calls here*/}
-      <code>
-        {key}: {data}
-      </code>
-    </div>
-  );
+// function Data(props) {
+//   const [selected, setSelected] = useState(true);
+//   let idx = props.idx;
+//   let data = props.data;
+//   let dataStyle = {
+//     borderTop: "1px solid #ccc",
+//     width: "12em",
+//     height: "2em",
+//     overflowY: "scroll",
+//     hover: "pointer",
+//     hoverColor: "blue",
+//     padding: "0.25em",
+//   };
+//   if (!selected) {
+//     dataStyle = {
+//       borderTop: "1px solid #ccc",
+//       backgroundColor: "darkblue",
+//       color: "white",
+//       width: "12em",
+//       height: "2em",
+//       overflowY: "scroll",
+//       hover: "pointer",
+//       hoverColor: "blue",
+//       padding: "0.25em",
+//     };
+//   }
+//   return (
+//     <div
+//       className="data"
+//       idx={idx}
+//       data={data}
+//       style={dataStyle}
+//       onClick={() => {
+//         setSelected(!selected);
+//         console.log(selected);
+//       }}
+//     >
+//       {/*Make DataBase API Calls here*/}
+//       <i>{data}</i>
+//     </div>
+//   );
+// }
+
+class Data extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: true,
+        };
+        this.changeSelected = this.changeSelected.bind(this);
+    }
+    changeSelected() {
+        this.setState({
+            selected: !this.state.selected
+        });
+        console.log(this.state.selected);
+    }
+    render() {
+        return (
+            <div
+                className="data"
+                idx={this.props.idx}
+                data={this.props.data}
+                style={this.state.selected ? {
+                    borderTop: "1px solid #ccc",
+                    width: "12em",
+                    height: "2em",
+                    overflowY: "scroll",
+                    hover: "pointer",
+                    hoverColor: "blue",
+                    padding: "0.25em",
+                } : {
+                    borderTop: "1px solid #ccc",
+                    backgroundColor: "darkblue",
+                    color: "white",
+                    width: "12em",
+                    height: "2em",
+                    overflowY: "scroll",
+                    hover: "pointer",
+                    hoverColor: "blue",
+                    padding: "0.25em",
+                }}
+                onClick={this.changeSelected}
+            >
+                {/*Make DataBase API Calls here*/}
+                <i>{this.props.data}</i>
+            </div>
+        )
+    }
 }
 
 export default Data;
--- parametric_search/src/components/parameter.js
+++ parametric_search/src/components/parameter.js
@@ -1,85 +1,29 @@
 import Data from "./data";
 import "./parameter.css";
-import React, { useState } from "react";
-// import { Component } from "react";
+// import React, { useState } from "react";
+import React, { Component } from "react";
 
-function Parameter(props) {
-  const [cleared, setCleared] = useState(false);
-  let name = props.name;
-  let data = [
-    "Diode",
-    "Transistor",
-    "Power Transistor",
-    "xx",
-    "xxx",
-    "xxxx",
-    "xxxxxx",
-    "xxxxxx",
-    "xxxxxxxx",
-  ]; // Query database
-  let dataList = [];
-  data.forEach((element, index) => {
-    dataList.push(<Data key={index} data={element}></Data>);
-  });
-  return (
-    <div
-      className="parameter"
-      style={{
-        border: "1px solid #ccc",
-        width: "12em",
-        height: "12em",
-        overflowY: "scroll",
-        overflowX: "hidden",
-      }}
-    >
-      <div className="parameter-heading">
-        <strong>{name}</strong>
-      </div>
-      <button
-        style={{
-          border: "0px solid #ccc",
-          height: "2em",
-          width: "100%",
-          overflowY: "scroll",
-          overflowX: "hidden",
-        }}
-        onClick={() => {
-          setCleared(true);
-          console.log(cleared);
-        }}
-      >
-        Clear Filter
-      </button>
-      <div>{dataList}</div>
-    </div>
-  );
-}
-
-// class Parameter extends Component {
-//     contructor(props) {
-//         super(props);
-//         this.state = {
-//             name: props.name,
-//             data: props.data,
-//         }
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     makeDataList() {
-//         let dataList = [];
-//         this.state.data.forEach((element, index) => {
-//             dataList.push(<Data key={index} data={element}></Data>);
-//         });
-//         return dataList;
-//     }
-
-//     handleClick() {
-//         console.log('Cleared');
-//     }
-
-//     render() {
-//         return (
-//             <div
+// function Parameter(props) {
+//   const [cleared, setCleared] = useState(true);
+//   let data = props.data; // Query database
+//   let name = props.name;
+// //   let data = [
+// //     "Diode",
+// //     "Transistor",
+// //     "Power Transistor",
+// //     "xx",
+// //     "xxx",
+// //     "xxxx",
+// //     "xxxxxx",
+// //     "xxxxxx",
+// //     "xxxxxxxx",
+// //   ];
+//   let dataList = [];
+//   data.forEach((element, index) => {
+//     dataList.push(<Data key={"1"+index} data={element}></Data>);
+//   });
+//   return (
+//     <div
 //       className="parameter"
 //       style={{
 //         border: "1px solid #ccc",
@@ -89,25 +33,86 @@ function Parameter(props) {
 //         overflowX: "hidden",
 //       }}
 //     >
-//       <div
-//         className="parameter-heading"
-//       >
-//         <strong>{this.state.name}</strong>
+//       <div className="parameter-heading">
+//         <strong>{name}</strong>
 //       </div>
-//       <button style={{
-//         border: "0px solid #ccc",
-//         height: '2em',
-//         width: '100%',
-//         overflowY: "scroll",
-//         overflowX: "hidden",
-//       }}
-//       onClick= {
-//         this.handleClick
-//       }>Clear Filter</button>
+//       <button
+//         style={{
+//           border: "0px solid #ccc",
+//           height: "2em",
+//           width: "100%",
+//           overflowY: "scroll",
+//           overflowX: "hidden",
+//         }}
+//         onClick={() => {
+//             setCleared(true);
+//             console.log(cleared);
+//         }}
+//       >
+//         Clear Filter
+//       </button>
 //       <div>{dataList}</div>
 //     </div>
-//         )
-//     }
+//   );
 // }
+
+class Parameter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cleared: true,
+    };
+    this.DataElement = React.createRef();
+    this.makeDataList = this.makeDataList.bind(this);
+  }
+
+  makeDataList() {
+    let dataList = [];
+    this.props.data.forEach((element, index) => {
+      dataList.push(<Data ref={this.DataElement} key={"1" + index} data={element}></Data>);
+    });
+    return dataList;
+  }
+
+  handleClick() {
+    this.setState({
+      cleared: !this.state.cleared,
+    });
+    console.log(this.cleared);
+    this.DataElement.current.changeSelected();
+  }
+
+  render() {
+    return (
+      <div
+        className="parameter"
+        style={{
+          border: "1px solid #ccc",
+          width: "12em",
+          height: "12em",
+          overflowY: "scroll",
+          overflowX: "hidden",
+        }}
+      >
+        <div className="parameter-heading">
+          <strong>{this.props.name}</strong>
+        </div>
+        <button
+          style={{
+            border: "0px solid #ccc",
+            height: "2em",
+            width: "100%",
+            overflowY: "scroll",
+            overflowX: "hidden",
+          }}
+          onClick={this.handleClick}
+        >
+          Clear Filter
+        </button>
+        <div>{this.makeDataList()}</div>
+      </div>
+    );
+  }
+}
 
 export default Parameter;
