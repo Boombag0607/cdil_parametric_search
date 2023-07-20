9b2634c1a30cd7412b587e3e5e7dbf69316f741a
Ananya Gautam <ananyagautam0607@gmail.com>
2023-07-20 22:46:40 +0530

    fix database file
    Signed-off-by: Ananya Gautam <ananyagautam0607@gmail.com>

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE cdillocal;
 
 CREATE TABLE device(
-    paramter_id SERIAL PRIMARY KEY,
-    description VARCHAR(255) NOT NULL 
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE parameters(
+    device_id SERIAL PRIMARY KEY,
+    parameters VARCHAR(255) NOT NULL
 );
