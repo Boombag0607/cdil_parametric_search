717b58083de2cd8f749197cc70fc584e80b2fab1
Ananya Gautam <ananyagautam0607@gmail.com>
2023-09-26 23:23:37 +0530

    add admin capabilities
    Signed-off-by: Ananya Gautam <ananyagautam0607@gmail.com>

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -20,7 +20,7 @@ import {
 // import MuiAccordionSummary from "@mui/material/AccordionSummary";
 // import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import BoltIcon from "@mui/icons-material/Bolt";
+// import BoltIcon from "@mui/icons-material/Bolt";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
@@ -98,58 +98,86 @@ function Landing() {
 
   return (
     <Box className="landing container m-4">
-      <Typography variant="h3" component="h1">
-        Parametric Search
+      <Typography variant="h3" component="h1" sx={{ mb: 4 }}>
+        Our Products
       </Typography>
-      <Typography className="lead">
+      <Box sx={{ height: "50vh", backgroundColor: "#eee", right: 0, left: 0 }}>
+        <Grid container sx={{ m: 1, p: 1 }}>
+          <Grid item>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repellendus distinctio corrupti nam nostrum! Recusandae
+            reprehenderit magnam asperiores possimus maiores delectus? Quae,
+            reprehenderit voluptates. Alias tempora corporis fugiat quasi
+            nesciunt distinctio.<br></br>
+          </Grid>
+          <Grid item>
+            <Button href="/search">Go to Main Search</Button>
+          </Grid>
+        </Grid>
+      </Box>
+      <Typography sx={{ m: 4 }}>
         This is a simple web app that allows you to explore data from the{" "}
         <Link href="https://www.cdil.com/" target="_blank" rel="noreferrer">
           CDIL
         </Link>{" "}
         website.
       </Typography>
-      {loading ? (
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress />
-        </Box>
-      ) : (
-        <Grid container spacing={2} className="m-1 p-1">
-          {categoryArray.map((categoryObject, categoryIndex) => (
-            <Grid item className="col" key={"col" + categoryIndex}>
-              <Accordion defaultExpanded>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography>{categoryObject.name}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Grid container direction="column">
-                    <List>
-                      {categoryObject.types.map((type, typeIdx) => (
-                        <ListItem disablePadding key={typeIdx}>
-                          <ListItemButton
-                            component={Link}
-                            to={`/search/${type.split(" ").join("_")}`}
-                          >
-                            <ListItemIcon>
-                              <BoltIcon />
-                              <ListItemText disableTypography primary={type} />
-                            </ListItemIcon>
-                          </ListItemButton>
-                        </ListItem>
-                      ))}
-                    </List>
-                    <Button href={`/${categoryObject.name}`}>Select All</Button>
-                  </Grid>
-                </AccordionDetails>
-              </Accordion>
-            </Grid>
-          ))}
-        </Grid>
-      )}
-      <Button href="/search">Go to Main Search</Button>
+      <Box>
+        <Typography component="" variant="h4" sx={{ m: 4 }}>
+          Browse By Category
+        </Typography>
+        {loading ? (
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Grid
+            container
+            spacing={2}
+            className="m-1 p-1"
+            // columns={{ xs: 1, sm: 2, md: 2, lg: 3 }}
+          >
+            {categoryArray.map((categoryObject, categoryIndex) => (
+              <Grid item className="col" key={"col" + categoryIndex}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography>{categoryObject.name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Grid container direction="column">
+                      <List>
+                        {categoryObject.types.map((type, typeIdx) => (
+                          <ListItem disablePadding key={typeIdx}>
+                            <ListItemButton
+                              component={Link}
+                              to={`/search/${type.split(" ").join("_")}`}
+                            >
+                              <ListItemIcon>
+                                {/* <BoltIcon /> */}
+                                <ListItemText
+                                  disableTypography
+                                  primary={type}
+                                />
+                              </ListItemIcon>
+                            </ListItemButton>
+                          </ListItem>
+                        ))}
+                      </List>
+                      <Button href={`/${categoryObject.name}`}>
+                        Select All
+                      </Button>
+                    </Grid>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
     </Box>
   );
 }
